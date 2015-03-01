1cc55683fe1216185ab1cda589e6c2b410a0735d
Dr. Hualiang Qi <hualiang.qi@gmail.com>
2015-03-01 18:57:08 +0100

    Adds spec implementation

--- spec/hqi_spec.rb
+++ spec/hqi_spec.rb
@@ -16,36 +16,45 @@ describe Hqi do
    describe '.transforms(data, till_midnight)' do
     it 'returns a MD5 hash of the provided data' do
       create_new_instance
-      expect(@id).to be_a String
-      expect(@id).to eq '24e73d3a4f027ff81ed4f32c8a9b8713'
+      expect(@id).to be_a(String)
+      expect(@id).to eq('24e73d3a4f027ff81ed4f32c8a9b8713')
     end
 
-#     it 'stores original and transformed data' do
-#       fail "Pending recommended to be used"
-#     end
-# 
-#     it 'restores the data to its original state after set time' do
-#       pending
-#     end
-#   end
-# 
-#   describe '.get(id)' do
-#     context 'before midnight (before time expired)' do
-#       it 'returns transformed data' do
-#         pending
-#       end
-#     end
-# 
-#     context 'past midnight (after time expired)' do
-#       it 'returns original data' do
-#         pending
-#       end
-#     end
-#   end
-# 
-#   describe '.midnight(id)' do
-#     it 'restores the data to its original state' do
-#       pending
-#     end
+     describe '.transforms(data, till_midnight)' do
+       it 'returns a md5 hash of the provided data' do
+         create_new_instance
+         expect(@id).to be_a(String)
+         expect(@id).to eq('24e73d3a4f027ff81ed4f32c8a9b8713')
+       end
+     end
+   
+     describe '.get(id)' do
+       context 'before midnight (before time expired)' do
+         it 'returns the transformed data' do
+           Hqi.stub(:check)
+           create_new_instance
+           expect(subject.get(@id)).to eq({ :key => 'VALUE' })
+         end
+       end
+   
+       context 'past midnight (after time expired)' do
+         it 'returns the original data' do
+           create_new_instance
+           Hqi.reset_data_at @id
+           expect(subject.get(@id)).to eq({ :key => 'value' })
+         end
+       end
+     end
+   
+     describe '.midnight(id)' do
+       context 'before midnight (before time expired)' do
+         it 'restores the data to its original state' do
+           Hqi.stub(:check)
+           create_new_instance
+           subject.midnight(@id)
+           expect(subject.get(@id)).to eq({ :key => 'value' })
+         end
+       end
+     end
   end
 end
