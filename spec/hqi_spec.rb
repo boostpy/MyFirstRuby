3b89c93f537cc1ba6ee5dfaf6ca19d45d93f428b
Dr. Hualiang Qi <hualiang.qi@gmail.com>
2015-03-01 11:55:21 +0100

    Adds Skeleton for rspec test suite

--- spec/hqi_spec.rb
+++ spec/hqi_spec.rb
@@ -1,7 +1,40 @@
 require 'spec_helper'
 
 describe Hqi do
-  it 'does stuff' do
-    pending # no code yet
-  end
-end
+  let(:data) {{ :key => 'value' }}
+   let(:till_midnight) { 0 }
+ 
+   describe '.transforms(data, till_midnight)' do
+     it 'returns a hash of the passed data' do
+       pending "To be implemented"
+     end
+ 
+     it 'stores original and transformed data' do
+       fail "Pending recommended to be used"
+     end
+ 
+     it 'restores the data to its original state after set time' do
+       pending
+     end
+   end
+ 
+   describe '.get(id)' do
+     context 'before midnight (before time expired)' do
+       it 'returns transformed data' do
+         pending
+       end
+     end
+ 
+     context 'past midnight (after time expired)' do
+       it 'returns original data' do
+         pending
+       end
+     end
+   end
+ 
+   describe '.midnight(id)' do
+     it 'restores the data to its original state' do
+       pending
+     end
+   end
+ end
